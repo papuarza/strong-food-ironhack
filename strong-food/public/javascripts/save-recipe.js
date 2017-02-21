b6ddf99e87c47a4836858dc1f72053225143fb80
Suazolc <suazolc@gmail.com>
2017-02-21 18:19:33 +0100

    done back

--- strong-food/public/javascripts/save-recipe.js
+++ strong-food/public/javascripts/save-recipe.js
@@ -11,23 +11,23 @@ function handleError(err) {
 }
 
 $(document).ready(function() {
-   var buttons = Array.from(document.querySelectorAll('#save-the-recipe'));
-   buttons.forEach(function(button) {
-     button.addEventListener('click', function(event) {
-         event.preventDefault();
-         let recipeId = document.getElementById("recipe-id").value;
-         console.log(recipeId);
-         $.ajax({
-             type: 'POST',
-             url: 'save-recipe',
-             data: {
-                 recipeId
-             },
-             success: showFeedback,
-             error: handleError
-         });
-         location.reload();
-     });
-   });
+    var buttons = Array.from(document.querySelectorAll('#save-the-recipe'));
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            let recipeId = document.getElementById("recipe-id").value;
+            console.log(recipeId);
+            $.ajax({
+                type: 'POST',
+                url: '/save-recipe',
+                data: {
+                    recipeId
+                },
+                success: showFeedback,
+                error: handleError
+            });
+            location.reload();
+        });
+    });
 
 });
